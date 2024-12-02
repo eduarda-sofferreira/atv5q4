710a15d911d6d4731a2b4c96e582feea86dc42ae
eduarda-sofferreira <espf1@aluno.ifal.edu.br>
2024-12-02 19:41:39 -0300

    Update q4.js

--- q4.js
+++ q4.js
@@ -1,3 +1,3 @@
 let saldo = parseFloat(prompt("Digite o saldo inicial:"));
 let saldoReajustado = saldo * 1.01;
-alert("Seu saldo com reajuste de 1% é: R$ {saldoReajustado.toFixed(2)}");
+alert("Seu saldo com reajuste de 1% é de:" + saldoReajustado);
